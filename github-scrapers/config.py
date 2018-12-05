73f2a4a11b7b63ea2211dfbfea2a68f5c9004c9e
ChicoXYC <1047547783@qq.com>
2018-12-05 18:33:02 +0800

    update GitHub activities

--- github-scrapers/config.py
+++ github-scrapers/config.py
@@ -1,3 +1,3 @@
 # Centralise the token update here
 # TODO: add notes on how to get and update token
-github_token = '81371300e7f76be054c5f8f72befdd1db142837c'
+github_token = 'c3d3db8e46d9d6ba80f6f887e9535e91482fb19e'

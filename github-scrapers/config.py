e4fbe697b0dd362a6a8cfde7129b57e95ee2aa2e
ChicoXYC <1047547783@qq.com>
2018-12-11 18:07:38 +0800

    update user activities json, desdriptions of activities

--- github-scrapers/config.py
+++ github-scrapers/config.py
@@ -1,3 +1,3 @@
 # Centralise the token update here
 # TODO: add notes on how to get and update token
-github_token = 'c3d3db8e46d9d6ba80f6f887e9535e91482fb19e'
+github_token = '6a7172f1c2775d629420be5ef98f4822a68d8d8d'

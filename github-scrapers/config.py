6497746b19dde6feeb685e54b1660e2159354191
ChicoXYC <1047547783@qq.com>
2018-12-16 21:01:53 +0800

    update data

--- github-scrapers/config.py
+++ github-scrapers/config.py
@@ -1,3 +1,3 @@
 # Centralise the token update here
 # TODO: add notes on how to get and update token
-github_token = '6a7172f1c2775d629420be5ef98f4822a68d8d8d'
+github_token = '8c45fcf62854847cc6f320172e5cf31be9bdc974'
